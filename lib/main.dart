baa7460b7eb603c4095ddf51a6be9e217cb89341
Francisco Freitas <fj.freitas@campus.fct.unl.pt>
2023-06-24 15:42:29 +0100

    First widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'calendar_timeline.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -101,6 +103,20 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            CalendarTimeline(
+              initialDate: DateTime(2020, 4, 20),
+              firstDate: DateTime(2019, 1, 15),
+              lastDate: DateTime(2020, 11, 20),
+              onDateSelected: (date) => print(date),
+              leftMargin: 20,
+              monthColor: Colors.blueGrey,
+              dayColor: Colors.teal[200],
+              activeDayColor: Colors.white,
+              activeBackgroundDayColor: Colors.redAccent[100],
+              dotsColor: Color(0xFF333A47),
+              selectableDayPredicate: (date) => date.day != 23,
+              locale: 'en_ISO',
             ),
           ],
         ),
