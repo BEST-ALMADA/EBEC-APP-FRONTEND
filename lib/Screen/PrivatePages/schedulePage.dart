6277226fbbbb6090a36ced87f227e26f6164eab9
franciscoabadesantos <fab.santos@campus.fct.unl.pt>
2023-07-11 00:35:16 +0100

    yolo

--- lib/Screen/PrivatePages/schedulePage.dart
+++ lib/Screen/PrivatePages/schedulePage.dart
@@ -1,3 +1,5 @@
+//https://pub.dev/documentation/table_calendar/latest/table_calendar/CalendarBuilders-class.html
+//https://pub.dev/packages/table_calendar
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
